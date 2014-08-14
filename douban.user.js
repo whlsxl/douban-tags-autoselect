5dcbc48070c9ada7ac6c1deefa2844439903e7c4
Wang Hailong <whlsxl@gmail.com>
2014-08-15 00:49:25 +0800

    存储选项状态

--- douban.user.js
+++ douban.user.js
@@ -3,10 +3,9 @@
 // @author whl
 // @description 豆瓣电影，自动添加标签
 // @version 0.1.0
-// @namespace http://userscripts.org/users/Kawaiiushio
-// @icon http://extensiondl.maxthon.cn/skinpack/17276781/1366787326/icons/icon_48.png
-// @include http://movie.douban.com/*
-// @run-at document-start
+// @namespace https://github.com/whlsxl/douban-rate-autoselect
+// @match http://movie.douban.com/*
+// @run-at document-end
 // ==/UserScript==
 
 (function() {
@@ -24,26 +23,24 @@
         if (!checked) {
           selectClass = 'rdact';
         }
+        console.log(parent.find('span.' + selectClass));
         parent.find('span.' + selectClass).trigger('click');
       };
     });
   }
 
-  $('.a_collect_btn,.collect_btn').each(function() {
-    $(this).click(function(){
+  $(document).ajaxComplete(function(event, xhr, settings) {
+    if (settings.url == "/j/subject/1418190/interest?interest=collect") {
       if(window.localStorage) {
-        console.log($('#advtags li.clearfix').length);
         $('#advtags li.clearfix').each(function() {
           var h = $(this);
           var auto_select = window.localStorage['auto_select' + h.attr('id')];
-          console.log('ssss');
-          console.log('auto_select' + h.attr('id'));
           console.log(auto_select);
-          if (!(auto_select === undefined)) {
-            $('.auto_select',h).attr('checked', window.localStorage['auto_select' + h.attr('id')]);
+          if (!(auto_select === undefined) && auto_select) {
+            $('.auto_select',h).click();
           }
         });
       }
-    });
+    };
   });
 })();
