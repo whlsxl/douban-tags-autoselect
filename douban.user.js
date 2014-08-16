f52fcd5b4e944951a5072330a364dac548a3ad6d
Wang Hailong <whlsxl@gmail.com>
2014-08-16 13:56:22 +0800

    基本可用

--- douban.user.js
+++ douban.user.js
@@ -5,42 +5,55 @@
 // @version 0.1.0
 // @namespace https://github.com/whlsxl/douban-rate-autoselect
 // @match http://movie.douban.com/*
-// @run-at document-end
+// @run-at document-idle
 // ==/UserScript==
 
-(function() {
-  var old_populate_tag_btns = populate_tag_btns;
-  populate_tag_btns = function(g, h, b, e) {
-    old_populate_tag_btns.apply(this, arguments);
-    var auto_select = '<input class="auto_select" type="checkbox">全选';
-    $('dt',h).append(auto_select);
-    $('.auto_select',h).change(function(event) {
-      if ($("#dialog").length) {
-        var selectClass = 'gract';
-        var parent = $(this).parents("li").eq(0);
-        var checked = $(this).is(":checked");
-        window.localStorage['auto_select' + parent.attr('id')] = checked;
-        if (!checked) {
-          selectClass = 'rdact';
-        }
-        console.log(parent.find('span.' + selectClass));
-        parent.find('span.' + selectClass).trigger('click');
-      };
-    });
-  }
+if(typeof isChromeExtension == 'undefined'){
+  addJQuery(init);
+}else{
+  function onReadyGM(){
+    addJQuery(init);
+  };
+};
 
-  $(document).ajaxComplete(function(event, xhr, settings) {
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//upcdn.b0.upaiyun.com/libs/jquery/jquery-2.0.3.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+function init() {
+  $(document).ajaxSuccess(function(event, xhr, settings) {
     if (settings.url == "/j/subject/1418190/interest?interest=collect") {
-      if(window.localStorage) {
-        $('#advtags li.clearfix').each(function() {
-          var h = $(this);
-          var auto_select = window.localStorage['auto_select' + h.attr('id')];
-          console.log(auto_select);
-          if (!(auto_select === undefined) && auto_select) {
-            $('.auto_select',h).click();
-          }
+      $('#advtags li.clearfix').each(function() {
+        var tagp = $(this);
+
+        var auto_select = '<input class="auto_select" type="checkbox">全选';
+        $('dt',tagp).append(auto_select);
+        $('.auto_select',tagp).change(function(event) {
+          if ($("#dialog").length) {
+            var selectClass = 'gract';
+            var parent = $(this).parents("li").eq(0);
+            var checked = $(this).is(":checked");
+            window.localStorage['auto_select' + parent.attr('id')] = checked;
+            if (!checked) {
+              selectClass = 'rdact';
+            }
+            console.log(parent.find('span.' + selectClass));
+            $('span.' + selectClass, parent).trigger('click');
+          };
         });
-      }
+
+        var last_auto_select = window.localStorage['auto_select' + tagp.attr('id')];
+        if (window.localStorage && !(last_auto_select === undefined) && last_auto_select) {
+          $('.auto_select',tagp).click();
+        }
+      });
     };
   });
-})();
+}
